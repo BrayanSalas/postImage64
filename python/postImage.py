cdc2b6a9eca0110dee242b752dbcbef1c096e405
BrayanSalas <brayansalasch@hotmail.com>
2018-07-28 14:38:53 -0500

    rasp

--- python/postImage.py
+++ python/postImage.py
@@ -1,6 +1,6 @@
 from firebase import firebase
 firebase = firebase.FirebaseApplication('https://proyecto-ejemplo-86c11.firebaseio.com/', None)
-
+    
 with open("../assets/goku.jpg", "rb") as f:
     data = f.read()
     imagenCodificada = data.encode("base64")
---
+++ python/postImageRasp.py
@@ -0,0 +1,18 @@
+from firebase import firebase
+import time
+import picamera
+firebase = firebase.FirebaseApplication('https://proyecto-ejemplo-86c11.firebaseio.com/', None)
+
+with picamera.PiCamera() as picam:
+	ruta = "../assets/foto.jpg"
+	picam.start_preview()
+	time.sleep(5)
+	picam.capture(ruta)
+	picam.stop_preview()
+	picam.close()
+	time.sleep(5)
+
+    with open(ruta, "rb") as f:
+        data = f.read()
+        imagenCodificada = data.encode("base64")
+        firebase.patch('/imagenes/', {'foto': imagenCodificada})
