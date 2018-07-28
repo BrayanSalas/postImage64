891eea4c6145710c2207a89ec58a1cbd3447b397
BrayanSalas <brayansalasch@hotmail.com>
2018-07-28 14:00:05 -0500

    Modified

--- python/postImage.py
+++ python/postImage.py
@@ -1,10 +1,8 @@
 from firebase import firebase
 firebase = firebase.FirebaseApplication('https://proyecto-ejemplo-86c11.firebaseio.com/', None)
 
-if __name__ == '__main__':
-    
-    with open("../assets/goku.jpg", "rb") as f:
-        data = f.read()
-        imagenCodificada = data.encode("base64")
+with open("../assets/goku.jpg", "rb") as f:
+    data = f.read()
+    imagenCodificada = data.encode("base64")
 
-    firebase.patch('/imagenes/', {'goku': imagenCodificada})
+firebase.patch('/imagenes/', {'gokuuu': imagenCodificada})
